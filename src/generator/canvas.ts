bc41b771827df0f1ce284c6d0e5448ba68a74432
king-prawns <giorgio.gamberoni@gmail.com>
2025-10-31 23:07:48 +0000

    canvas generator

---
+++ src/generator/canvas.ts
@@ -0,0 +1,20 @@
+
+const generate = (): VideoFrame | null => {
+    const canvas = document.createElement('canvas');
+    // draw something on the canvas
+    const ctx = canvas.getContext('2d');
+
+    if (!ctx) {
+        return null;
+    }
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, 100, 100);
+    
+    const videoFrame = new VideoFrame(canvas, { timestamp: 0 });
+
+    return videoFrame;
+}
+
+
+export default generate;
